8b6b9004fb9a989ee2c8b1c534f5b86788ca0255
Neko Ayaka <neko@ayaka.moe>
2024-10-07 19:43:05 +0800

    fix: path
    Signed-off-by: Neko Ayaka <neko@ayaka.moe>

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,10 +1,5 @@
-<script setup lang="ts">
-const { backgroundColor } = useAppBackground()
-</script>
-
 <template>
   <div
-    :style="{ backgroundColor }"
     class="mx-auto h-full max-w-[1200px] w-full flex flex-col"
     transition="all ease-in-out" duration-1000
   >
